488434ff577c51854e37cdb72ace6fe70a973be3
Ana de Barros <scralowrealow@gmail.com>
2016-12-09 22:48:18 +0000

    first version.2

--- js/main.js
+++ js/main.js
@@ -1,25 +1,21 @@
 //home page
 
-$(document).ready(function(){
-	setInterval ("pisca()", 600);
+$(document).ready(function() {
+    setInterval("pisca()", 600);
 });
 
 
 
 function pisca() {
-	$("#pisca"). animate({
-		opacity:0
-	}, "fast","swing").animate({
-		opacity:1
-	}, "fast", "swing");
-}
+    $("#pisca").animate({
+        opacity: 0
+    }, "fast", "swing").animate({
+        opacity: 1
+    }, "fast", "swing");
+};
 
-//about me nav bar
-
-/*
+//for toggle change
 
 function cross(x) {
-x.classList.toggle("change");
-}
-
-*/
+    x.classList.toggle("change");
+};
